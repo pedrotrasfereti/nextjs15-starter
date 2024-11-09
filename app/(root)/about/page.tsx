d40d68d2091641d2a390156e7b1f7d3e577b8fde
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-09 18:24:20 -0300

    feat: implement error boundary with error.tsx

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Page = () => {
+  throw new Error('HELP THE KILLER IS ESCAPING HELLLPPPPP!!!!!!!!!');
+
   return (
     <div>
       <h1 className="text-3xl font-black">About</h1>
