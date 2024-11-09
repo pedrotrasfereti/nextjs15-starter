aeb983670eb1d253605362b5eb42c10121eb94f1
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-09 18:57:55 -0300

    feat: implement global error boundary with global-error.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const Home = () => {
+  throw new Error('HELP THE KILLER IS ESCAPING HELLLPPPPP!!!!!!!!!');
+
   return (
     <div>
       <h1 className="text-3xl font-black">Welcome to Next.js 15</h1>
---
+++ app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client'; // Error boundaries must be Client Components
+ 
+const GlobalError = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) => {
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
