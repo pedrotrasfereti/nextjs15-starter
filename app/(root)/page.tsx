52be70b86ab64658347754ad2303f0d80ec72da8
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-17 18:50:08 -0300

    feat: fetch hello world message from backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const response = await fetch("http://localhost:3000/api");
+  const data = await response.json();
+
   return (
     <div>
       <h1 className="text-3xl font-black">Welcome to Next.js 15</h1>
-      <h2 className="text-xl">Hello</h2>
+      <h2 className="text-xl">{ data.message }</h2>
     </div>
   );
 };
