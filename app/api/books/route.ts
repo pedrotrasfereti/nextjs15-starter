8259d2104714b53b9adb0b41516fd7df635f3b71
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-17 18:56:47 -0300

    feat(api/books): add POST route

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -4,3 +4,9 @@ import books from "@/app/api/books/db";
 export async function GET() {
   return Response.json(books);
 }
+
+export async function POST(request: Request) {
+  const book = await request.json();
+  books.push(book);
+  return Response.json(books);
+}
