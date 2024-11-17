6d7216ddfa8bbb5d2a242dad6ad53e482120c9f4
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-17 19:04:45 -0300

    style(api/books): format file

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -1,29 +1,28 @@
 // Two routes: PUT and DELETE
+import books from "@/app/api/books/db";
 
-  import books from "@/app/api/books/db";
+// Update request
+export async function PUT(
+  request: Request,
+  context: { params: { id: string }},
+) {
+  const id = +context.params.id;
+  const book = await request.json();
 
-  // Update request
-  export async function PUT(
-    request: Request,
-    context: { params: { id: string }},
-  ) {
-    const id = +context.params.id;
-    const book = await request.json();
+  const index = books.findIndex((b) => b.id === id);
+  books[index] = book;
 
-    const index = books.findIndex((b) => b.id === id);
-    books[index] = book;
+  return Response.json(books);
+}
 
-    return Response.json(books);
-  }
+// Delete request
+export async function DELETE(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const id = +context.params.id;
 
-  // Delete request
-  export async function DELETE(
-    request: Request,
-    context: { params: { id: string } }
-  ) {
-    const id = +context.params.id;
-
-    const index = books.findIndex((b) => b.id === id);
-    books.splice(index, 1);
-    return Response.json(books);
-  }
+  const index = books.findIndex((b) => b.id === id);
+  books.splice(index, 1);
+  return Response.json(books);
+}
