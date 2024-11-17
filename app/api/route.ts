bd7581887de0b53384b6470f9e7fee91a2f0199a
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-11-17 18:49:11 -0300

    fix(api): remove json.stringify from response message

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
   return Response.json(
-    JSON.stringify({ message: 'Hello World from Backend!'}),
+    { message: 'Hello World from Backend!'},
     {
       headers: { 'Content-Type': 'application/json' },
     },
